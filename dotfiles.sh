a2ccdc7c2750e7da24a8d8075177d22c4ca9629f
Jordi Blasco Pallares <jordi.blasco@gmail.com>
2016-06-10 20:33:48 +1200

    minor change

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
+cp .alias ~/.alias
 cp -pr .byobu ~/
 echo "test -s ~/.alias && . ~/.alias || true" >> ~/.bashrc
 git config --global alias.co checkout
