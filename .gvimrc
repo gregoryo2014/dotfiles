1565f898f11b19ba362b5369363784e0272638d0
Gregory Orange <8380355+gregoryo2014@users.noreply.github.com>
2020-11-19 08:43:50 +0800

    Add .gvimrc font

---
+++ .gvimrc
@@ -0,0 +1,1 @@
+set guifont=h16_b
--- dotfiles.sh
+++ dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
+cp .gvimrc ~/.gvimrc
 vim +PluginInstall +qall
 cp .alias ~/.alias
 cp -pr .byobu ~/
